1b1369070d535b6b86a938a22aad4c102e9131f5
lgs00 <65650724+lgs00@users.noreply.github.com>
2022-05-16 16:09:09 +0800

    Update particles.js

--- static/js/particles.js
+++ static/js/particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 400,
+        value: 380,
         density: {
           enable: true,
           value_area: 800
@@ -44,16 +44,16 @@ var pJS = function(tag_id, params){
           height: 100
         }
       },
-      opacity: {
-        value: 1,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 2,
-          opacity_min: 0,
-          sync: false
-        }
-      },
+      "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
       size: {
         value: 3,
         random: false,
@@ -66,14 +66,14 @@ var pJS = function(tag_id, params){
       },
       line_linked: {
         enable: true,
-        distance: 100,
+        distance: 150,
         color: '#44546A',
         opacity: 0.4,
         width: 1
       },
       move: {
         enable: true,
-        speed: 2,
+        speed: 4,
         direction: 'none',
         random: false,
         straight: false,
@@ -81,8 +81,8 @@ var pJS = function(tag_id, params){
         bounce: false,
         attract: {
           enable: false,
-          rotateX: 3000,
-          rotateY: 3000
+          rotateX: 600,
+          rotateY: 1200
         }
       },
       array: []
@@ -102,16 +102,18 @@ var pJS = function(tag_id, params){
       },
       modes: {
         grab:{
-          distance: 100,
+          distance: 140,
           line_linked:{
             opacity: 1
           }
         },
-        bubble:{
-          distance: 200,
-          size: 80,
-          duration: 0.4
-        },
+        "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
         repulse:{
           distance: 200,
           duration: 0.4
