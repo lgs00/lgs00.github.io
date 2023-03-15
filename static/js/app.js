b2f0c9af98902cfe33319494b3671f0a0e34e9f3
lgs00 <lgs0000@mail.dlut.edu.cn>
2023-03-15 21:17:52 +0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -45,7 +45,7 @@ particlesJS('particles-js',
         "random": false,
         "anim": {
           "enable": false,
-          "speed": 1,
+          "speed": 3,
           "opacity_min": 0.1,
           "sync": false
         }
